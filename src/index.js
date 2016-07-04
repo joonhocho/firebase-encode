4e28fabd387acdb58966efbc86980481a3cf3abc
Joon Ho Cho <joonho1101@gmail.com>
2016-07-03 19:21:00 -0700

    encodeComponents / decodeComponents

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 // http://stackoverflow.com/a/6969486/692528
 const escapeRegExp = (str) => str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 
+
+const create = (chars) => {
+  const charCodes = chars.map((c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`);
+
+  const charToCode = {};
+  const codeToChar = {};
+  chars.forEach((c, i) => {
+    charToCode[c] = charCodes[i];
+    codeToChar[charCodes[i]] = c;
+  });
+
+  const charsRegex = new RegExp(`[${escapeRegExp(chars.join(''))}]`, 'g');
+  const charCodesRegex = new RegExp(charCodes.join('|'), 'g');
+
+  const encode = (str) => str.replace(charsRegex, (match) => charToCode[match]);
+  const decode = (str) => str.replace(charCodesRegex, (match) => codeToChar[match]);
+
+  return {encode, decode};
+};
+
+
 // http://stackoverflow.com/a/19148116/692528
-const chars = '.$[]#/%'.split('');
-const charCodes = chars.map((c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`);
+const {
+  encode,
+  decode,
+} = create('.$[]#/%'.split(''));
 
-const charToCode = {};
-const codeToChar = {};
-chars.forEach((c, i) => {
-  charToCode[c] = charCodes[i];
-  codeToChar[charCodes[i]] = c;
-});
 
-const charsRegex = new RegExp(`[${escapeRegExp(chars.join(''))}]`, 'g');
-const charCodesRegex = new RegExp(charCodes.join('|'), 'g');
-
-const encode = (str) => str.replace(charsRegex, (match) => charToCode[match]);
-const decode = (str) => str.replace(charCodesRegex, (match) => codeToChar[match]);
+// Without `/`.
+const {
+  encode: encodeComponents,
+  decode: decodeComponents,
+} = create('.$[]#%'.split(''));
 
 
 export {
+  create,
   encode,
   decode,
+  encodeComponents,
+  decodeComponents,
 };
 
 export default {
+  create,
   encode,
   decode,
+  encodeComponents,
+  decodeComponents,
 };
--- test/index.js
+++ test/index.js
@@ -1,6 +1,11 @@
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
-import {encode, decode} from '../lib';
+import {
+  encode,
+  decode,
+  encodeComponents,
+  decodeComponents,
+} from '../lib';
 
 
 describe('encode / decode', () => {
@@ -18,5 +23,32 @@ describe('encode / decode', () => {
 
     expect(/[\.\$\[\]#\/]/.test(encoded)).to.be.false;
     expect(decode(encoded)).to.equal(str);
+  });
+});
+
+
+describe('encodeComponents / decodeComponents', () => {
+  it('allows /', () => {
+    const str = '02/10/2013';
+    const encoded = encodeComponents(str);
+
+    expect(encoded).to.equal(str);
+    expect(decodeComponents(encoded)).to.equal(str);
+  });
+
+  it('encodes and decodes ascii', () => {
+    const str = '02/10/2013.$[]#/%234_-!@#$%^&*()0];:\'"`\\=/?+|';
+    const encoded = encodeComponents(str);
+
+    expect(/[\.\$\[\]#]/.test(encoded)).to.be.false;
+    expect(decodeComponents(encoded)).to.equal(str);
+  });
+
+  it('encodes and decodes unicodes', () => {
+    const str = '¥¼Ññ⼽파あア㆚㒅';
+    const encoded = encodeComponents(str);
+
+    expect(/[\.\$\[\]#]/.test(encoded)).to.be.false;
+    expect(decodeComponents(encoded)).to.equal(str);
   });
 });
